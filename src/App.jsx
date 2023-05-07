b817b00c065486a97e47db347cd411edb11ec4d8
Rebecca <bsoza93@gmail.com>
2023-05-07 11:18:44 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,28 @@
 import "./css/Style.css";
 import Home from "./pages/Home";
 import DarkMode from "./utils/DarkMode";
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { Flex, Box, Spacer } from "@chakra-ui/layout";
 import Navbar from "./pages/Navbar";
 
 function App() {
-  const [windowDimension, detectHW] = useState ({
+  const [windowDimension, detectHW] = useState({
     winWidth: window.innerWidth,
     winHeight: window.innerHeight,
   });
 
   const detectSize = () => {
     detectHW({
-    winWidth: window.innerWidth,
-    winHeight: window.innerHeight,
+      winWidth: window.innerWidth,
+      winHeight: window.innerHeight,
     });
   };
 
   useEffect(() => {
-    window.addEventListener('resize', detectSize);
-    return() => {
-      window.removeEventListener('resize', detectSize);
-    }
+    window.addEventListener("resize", detectSize);
+    return () => {
+      window.removeEventListener("resize", detectSize);
+    };
   });
 
   const height = windowDimension.winHeight - 80;
@@ -33,18 +33,16 @@ function App() {
     <>
       <div className="container">
         <DarkMode />
-        <div className="content"
-        style={{width: width, height: height}}>
-        <Flex>
-          <Box>
-            <Navbar />
-          </Box>
-          <Spacer />
-          
-      <Box>
-            <Home />
-          </Box>
-        </Flex>
+        <div className="content" style={{ width: width, height: height }}>
+          <Flex>
+            <Box>
+              <Navbar />
+            </Box>
+            <Spacer />
+            <Box>
+              <Home />
+            </Box>
+          </Flex>
         </div>
       </div>
     </>
