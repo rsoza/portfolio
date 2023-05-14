ce52614bf5086ac2545ebf84868043f118ea61b0
Rebecca <bsoza93@gmail.com>
2023-05-13 22:22:23 -0500

    uh

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -17,26 +17,23 @@ function Contact() {
       animate={{ opacity: 1}}
       exit={{ opacity: 0 }}
     >
-        <motion.div
-        initial={{ opacity: 0, y: 100 }}
-        animate={{
-          opacity: 1,
-          y: 0,
-          transition: { type: "spring", delay: 0.5, duration: 1 },
-        }}
-        exit={{ opacity: 0, y: 100 }}
-        style={{
-          position: "relative",
-          height: "60vh",
-        }}
-      >
-        <img
-          src={pic}
-          alt="profile"
-          className="profile"
-          style={{ position: "absolute"}}
-        />
-      </motion.div>
+
+    <motion.div
+      initial={{ opacity: 0, y: 100}}
+      animate={{
+        opacity: 1, y: 0,
+        transition: { type: "spring", delay: 0.5, duration: 1 },
+      }}
+      exit={{ opacity: 0, y:100}}
+      style={{position: "relative", height: '98vh'}}
+    >
+      <img
+        src={pic}
+        alt="profile"
+        className="profile"
+      />
+    </motion.div>
+
       <div className="info">
           <AnimatedTextWord text={"Hello,"} />
         <p>lets get to know each other!</p>
--- src/utils/AnimatedRoutes.js
+++ src/utils/AnimatedRoutes.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { Route, Routes, useLocation } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
 import "../css/index.css";
@@ -9,7 +9,8 @@ import Experience from "../pages/Experience";
 import Contact from "../pages/Contact";
 import DarkMode from "../components/DarkMode";
 import Navbar from "./Navbar";
-import LoadingScreen from '../pages/LoadingScreen';
+import LoadingScreen from "../pages/LoadingScreen";
+
 
 function AnimatedRoutes() {
   const location = useLocation();
@@ -21,35 +22,42 @@ function AnimatedRoutes() {
     }, 2000);
   }, []);
 
-
   return (
     <AnimatePresence>
-      {isLoading ? <LoadingScreen /> : 
+      {isLoading ? (
+        <LoadingScreen />
+      ) : (
         <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}
-        transition={{ duration: 1, delay: 0.5 }}>
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 1, delay: 0.5 }}
+        >
           <div id="Container">
-          <div className="air air1"></div>
-          <div className="air air2"></div>
-          <div className="air air3"></div>
-          <div className="air air4"></div>
-        </div><div className="borders" id="Borders">
+            <div className="air air1"></div>
+            <div className="air air2"></div>
+            <div className="air air3"></div>
+            <div className="air air4"></div>
+          </div>
+          <div className="borders" id="Borders">
             <div className="border_top" />
             <div className="border_bot" />
-          </div><div className="frame" id="Frame">
+          </div>
+          <div className="frame" id="Frame">
             <div className="frame_border frame_left" />
             <div className="frame_border frame_right" />
             <div className="frame_border frame_top" />
             <div className="frame_border frame_bot" />
-          </div><Navbar /><DarkMode />
+          </div>
+          <Navbar />
+          <DarkMode />
           <Routes location={location} key={location.pathname}>
             <Route exact path="/" element={<Home />} />
             <Route exact path="/projects" element={<Experience />} />
             <Route exact path="/contact" element={<Contact />} />
-          </Routes></motion.div>
-}
+          </Routes>
+        </motion.div>
+      )}
     </AnimatePresence>
   );
 }
