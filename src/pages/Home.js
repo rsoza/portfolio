49042de2431f626b5a169a7efbd2ceb3e21baf66
Rebecca <bsoza93@gmail.com>
2023-11-16 20:06:18 -0600

    resume

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,17 +11,17 @@ function Home() {
         <p className="home_aboutme">
           <span>Originally from Miami,</span>
           <span>Florida, moved to Kansas</span>
-          <span>in early 2021 after</span>
-          <span>completing four years</span>
-          <span>in the Coast Guard to</span>
-          <span>completing a CS degree at</span>
+          <span>in early 2021 after completing</span>
+          <span>four years in the Coast Guard</span>
+          <span>to completing a CS degree at</span>
           <span>Wichita State University. </span>
-          <span>This virtual business</span>
-          <span>card was made using </span>
-          <span>Firebase and React JS</span>
-          <span>by me. Currently,</span>
-          <span>pursuing a position in</span>
-          <span>Software Development as</span>
+          <span>This portfolio was insprired</span>
+          <span>with the idea of having a </span>
+          <span>virtual business card which</span>
+          <span>was made by me using </span>
+          <span>Firebase and React JS.</span>
+          <span>Currently, pursuing a position</span>
+          <span>in Software Development as</span>
           <span>soon as I graduate in</span>
           <span>December 2023.</span>
         </p>
